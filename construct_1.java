c18dce5ee3a6dcd15feedb8f9836a485eec4d3c6
Tripti Vaish <trptivaish@gmail.com>
2020-06-14 19:23:32 +0530

    CONSTRUCTOR PRGMS

---
+++ construct_1.java
@@ -0,0 +1,18 @@
+
+class construct_1
+{
+    int p,t;
+    double r,SI;
+     
+    construct_1()
+    {
+        p=1000;
+        t=2;
+        r=5.0;
+        SI=(p*r*t)/100;
+    }
+    public static void main()
+    {
+        construct_1 obj=new construct_1();
+    }
+}
---
+++ construct_2.java
@@ -0,0 +1,16 @@
+
+class construct_2
+{
+double radius,area;
+final double pi=3.14;
+
+ construct_2()
+{
+radius=3.5;
+area=pi*radius*radius;
+}
+public static void main()
+{
+    construct_2 obj=new construct_2();
+}
+}
